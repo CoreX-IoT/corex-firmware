d7ebded9e2db448b2fdbbaaf188c93189ccfc527
Sendy Meiladi <sendymeiladi.28@gmail.com>
2023-11-09 13:32:19 +0700

    change contents samples folder

--- examples/Espressif/Send/Connection.h
+++ examples/Espressif/Send/Connection.h
@@ -1,22 +1,22 @@
 WiFiClient net;
-Dashboard dashboard;
+CoreX corex;
 
-void setupDashboard(String authProject, String clientId) {
-  Serial.println("Dashboard IoT by Nusabot");
+void setupCorex() {
+  Serial.println("CoreX IoT by Nusabot");
   Serial.println("Menghubungkan ke WiFi");
   while (WiFi.status() != WL_CONNECTED) {
     Serial.print(".");
     delay(1000);
   }
 
+
   Serial.println("\nTerhubung ke WiFi!");
   Serial.print("Menghubungkan ke server/broker");
-  while (!dashboard.connect(clientId.c_str())) {
+  while (!corex.connect()) {
     Serial.print(".");
     delay(1000);
   }
 
   Serial.println("\nTerhubung ke server!");
 
-  dashboard.subscribe(authProject+"/data/#");
 }
