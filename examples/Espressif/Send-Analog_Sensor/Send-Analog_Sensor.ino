314ce890a6ab99a9db0b20e50a2574bb6f6a2c49
Lorenz Adam Damara <lorenzrenz@gmail.com>
2023-11-10 21:48:40 +0700

    remove server parameter and send data type

--- examples/Espressif/Send-Analog_Sensor/Send-Analog_Sensor.ino
+++ examples/Espressif/Send-Analog_Sensor/Send-Analog_Sensor.ino
@@ -1,27 +1,26 @@
 #include <CoreX.h>
 #include "Connection.h"
 
+// Ubah nilai auth_token dan device anda.
 const char* AUTH_TOKEN = "..........";
 const char* DEVICE_ID = "..........";
 
-#define SENSOR_PIN 14  // GPIO D5 sesuaikan pin yang ingin Anda gunakan 
+#define SENSOR_PIN A0
 CoreXTimer timer;     // Gunakan timer agar dapat mengeksekusi perintah setiap sekian milidetik tanpa blocking.
 
 // Ubah nilai berikut sesuai jaringan Anda.
 const char ssid[] = "..........";
 const char pass[] = "..........";
 
-const char server[] = "nusabotid.cloud.shiftr.io";
-
 void send() {
   int sensorValue = analogRead(SENSOR_PIN);
-  corex.send("sensor", String(sensorValue).c_str());
-  }
+  corex.send("sensor", sensorValue);
+}
 
 void setup() {
   Serial.begin(115200);
   WiFi.begin(ssid, pass);
-  corex.begin(server, net);
+  corex.begin(net);
 
   timer.setInterval(1000, send);     // Lakukan publish setiap 1000 milidetik.
 
@@ -30,8 +29,8 @@ void setup() {
 
 void loop() {
   corex.loop();
-  //delay(10);                          // Hapus komentar untuk memberikan delay 10 milidetik jika terjadi kendala pada stabilitas WiFi.
   timer.run();                          // Jalankan timer.
+  //delay(10);                          // Hapus komentar untuk memberikan delay 10 milidetik jika terjadi kendala pada stabilitas WiFi.
 
   // Periksa apakah perangkat masih terhubung.
   if (!corex.connected()) {
